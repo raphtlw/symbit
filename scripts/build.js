baee70add05537851fe9d9a2d1f4dc9aa9c72b6f
Raphael Tang <raphpb1912@gmail.com>
2020-09-07 20:31:45 +0800

    build bugfix

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,9 @@
 const shell = require("shelljs");
 const fs = require("fs");
 
-function exec(command) {
-  const execResult = shell.exec(command);
-  console.log(execResult.stdout);
-}
-
 fs.rmdirSync("dist", { recursive: true });
-exec("tsc --outDir __build");
-exec("pkg __build/main.js --out-path dist/");
+shell.exec("tsc --outDir __build");
+shell.exec("pkg __build/main.js --out-path dist/");
 fs.rmdirSync("__build", { recursive: true });
 fs.renameSync("dist/main-linux", "dist/symbit-linux");
 fs.renameSync("dist/main-macos", "dist/symbit-macos");
