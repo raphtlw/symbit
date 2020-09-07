9ae01a923d04b7e3188719dd5e5781dd82484257
Raphael Tang <raphpb1912@gmail.com>
2020-09-07 20:30:19 +0800

    improve build process

---
+++ scripts/build.js
@@ -0,0 +1,17 @@
+const shell = require("shelljs");
+const fs = require("fs");
+
+function exec(command) {
+  const execResult = shell.exec(command);
+  console.log(execResult.stdout);
+}
+
+fs.rmdir("dist", { recursive: true }, (err) => {
+  if (err) throw err;
+});
+exec("tsc --outDir __build");
+exec("pkg __build/main.js --out-path dist/");
+fs.rmdirSync("__build", { recursive: true });
+fs.renameSync("dist/main-linux", "dist/symbit-linux");
+fs.renameSync("dist/main-macos", "dist/symbit-macos");
+fs.renameSync("dist/main-win.exe", "dist/symbit-windows.exe");
