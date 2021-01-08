13209565ab788c6c2a1b0b825e055bdc6e128f02
Raphael Tang <raphpb1912@gmail.com>
2021-01-08 20:35:33 +0800

    Added macOS maker

--- forge.config.js
+++ forge.config.js
@@ -1,33 +1,40 @@
-const path = require('path');
+const path = require("path")
 
-const windowsIcon = path.join(__dirname, 'public', 'icons', 'win', 'icon.ico');
-const linuxIcon = path.join(__dirname, 'public', 'icons', 'png', '512x512.png');
+const windowsIcon = path.join(__dirname, "public", "icons", "win", "icon.ico")
+const linuxIcon = path.join(__dirname, "public", "icons", "png", "512x512.png")
+const macIcon = path.join(__dirname, "public", "icons", "mac", "icon.icns")
 
 /** @type {import('@electron-forge/shared-types').ForgeConfig} */
 module.exports = {
   packagerConfig: {
-    executableName: 'symbit',
+    executableName: "symbit",
   },
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: { name: 'symbit', iconUrl: windowsIcon, setupIcon: windowsIcon },
+      name: "@electron-forge/maker-squirrel",
+      config: { name: "symbit", iconUrl: windowsIcon, setupIcon: windowsIcon },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'linux'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "linux"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {
         icon: linuxIcon,
       },
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {
         icon: linuxIcon,
+      },
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        icon: macIcon,
       },
     },
   ],
-};
+}
